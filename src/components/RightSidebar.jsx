75db9c990163bde13ce22b9d861cba13b370656f
Mike Ta <h.michael.ta@gmail.com>
2020-10-06 10:40:30 +0200

    added exitUrls to contact info

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -3,12 +3,15 @@ import './RightSidebar.css'
 
 class RightSidebar extends Component {
     render() {
+        let github = "https://github.com/miketaart";
+        let linkedIn = "https://www.linkedin.com/in/michael-ta-3796756a/";
+        let email  = "mailto: h.michael.ta@gmail.com";
         return (
             <div className="rightSidebar">
                 <ul>
-                    <li>GITHUB</li>
-                    <li>LINKEDIN</li>
-                    <li>H.MICHAEL.TA@GMAIL.COM</li>
+                    <li><a href = {github} target="_blank" rel="noopener noreferrer">GITHUB</a></li>
+                    <li><a href = {linkedIn} target="_blank" rel="noopener noreferrer">LINKEDIN</a></li>
+                    <li><a href = {email} target="_blank" rel="noopener noreferrer">H.MICHAEL.TA@GMAIL.COM</a></li>
                 </ul>
             </div>
         );
